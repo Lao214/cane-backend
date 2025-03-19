007decdb556d61d164e49af9b65d6ab4530568ea
echoes <1085444086@qq.com>
2025-03-19 22:27:54 +0800

    结构修改

--- src/main/java/com/example/integratedHub/entity/BCaneSensitivity.java
+++ src/main/java/com/example/integratedHub/entity/BCaneSensitivity.java
@@ -26,7 +26,7 @@ public class BCaneSensitivity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-      @TableId(value = "id", type = IdType.ID_WORKER)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     //@ApiModelProperty(value = "品种名称")
@@ -34,6 +34,12 @@ public class BCaneSensitivity implements Serializable {
 
     //@ApiModelProperty(value = "品种抗寒/旱/黑穗病级别")
     private String resistanceLevel;
+
+    //@ApiModelProperty(value = "品种抗旱级别")
+    private String resistanceLevelHan;
+
+    //@ApiModelProperty(value = "品种抗黑穗病级别")
+    private String resistanceLevelHei;
 
     //@ApiModelProperty(value = "毛状体")
     private String trichome;
@@ -81,7 +87,7 @@ public class BCaneSensitivity implements Serializable {
     private String controlPlan;
 
     //@ApiModelProperty(value = "参考文献")
-    private String references;
+    private String caneReferences;
 
     //@ApiModelProperty(value = "创建时间")
     private Date createTime;
@@ -100,5 +106,100 @@ public class BCaneSensitivity implements Serializable {
     @TableField(exist = false)
     private String sortType;
 
+    //@ApiModelProperty(value = "毛状体")
+    private String trichomeHan;
+
+    //@ApiModelProperty(value = "芽型")
+    private String budTypeHan;
+
+    //@ApiModelProperty(value = "茎")
+    private String stemHan;
+
+    //@ApiModelProperty(value = "叶鞘")
+    private String leafSheathHan;
+
+    //@ApiModelProperty(value = "表皮细胞")
+    private String epidermalCellsHan;
+
+    //@ApiModelProperty(value = "楚质层、角质层")
+    private String cuticleStratumCorneumHan;
+
+    //@ApiModelProperty(value = "硅细胞")
+    private String siliconCellHan;
+
+    //@ApiModelProperty(value = "栓细胞")
+    private String thrombocytesHan;
+
+    //@ApiModelProperty(value = "气孔器")
+    private String stomataHan;
+
+    //@ApiModelProperty(value = "叶绿体")
+    private String chloroplastHan;
+
+    //@ApiModelProperty(value = "线粒体")
+    private String mitochondriaHan;
+
+    //@ApiModelProperty(value = "抗逆性化学物质")
+    private String stressResistantChemicalsHan;
+
+    //@ApiModelProperty(value = "抗逆性酶")
+    private String stressResistanceEnzymeHan;
+
+    //@ApiModelProperty(value = "相关基因")
+    private String relatedGenesHan;
+
+    //@ApiModelProperty(value = "调控方案")
+    private String controlPlanHan;
+
+    //@ApiModelProperty(value = "参考文献")
+    private String caneReferencesHan;
+
+    //@ApiModelProperty(value = "毛状体")
+    private String trichomeHei;
+
+    //@ApiModelProperty(value = "芽型")
+    private String budTypeHei;
+
+    //@ApiModelProperty(value = "茎")
+    private String stemHei;
+
+    //@ApiModelProperty(value = "叶鞘")
+    private String leafSheathHei;
+
+    //@ApiModelProperty(value = "表皮细胞")
+    private String epidermalCellsHei;
+
+    //@ApiModelProperty(value = "楚质层、角质层")
+    private String cuticleStratumCorneumHei;
+
+    //@ApiModelProperty(value = "硅细胞")
+    private String siliconCellHei;
+
+    //@ApiModelProperty(value = "栓细胞")
+    private String thrombocytesHei;
+
+    //@ApiModelProperty(value = "气孔器")
+    private String stomataHei;
+
+    //@ApiModelProperty(value = "叶绿体")
+    private String chloroplastHei;
+
+    //@ApiModelProperty(value = "线粒体")
+    private String mitochondriaHei;
+
+    //@ApiModelProperty(value = "抗逆性化学物质")
+    private String stressResistantChemicalsHei;
+
+    //@ApiModelProperty(value = "抗逆性酶")
+    private String stressResistanceEnzymeHei;
+
+    //@ApiModelProperty(value = "相关基因")
+    private String relatedGenesHei;
+
+    //@ApiModelProperty(value = "调控方案")
+    private String controlPlanHei;
+
+    //@ApiModelProperty(value = "参考文献")
+    private String caneReferencesHei;
 
 }
